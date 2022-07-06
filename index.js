af992599d9df8dc1238adbc1517396aec56b6b6a
Yeshu <yeswanth@flutura.com>
2022-07-06 14:33:06 +0530

    phonebook app step4 complete

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
 app.get("/info", (request, response) => {
   const htmlResponse = `
     <p>Phonebook has info for ${persons.length} people</p>
