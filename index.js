0c631af9027ddd938786c26e29bd11259cf03ab5
Yeshu <yeswanth@flutura.com>
2022-07-06 16:01:06 +0530

    phonebook app step8 complete

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
+
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[Content-Length] :response-time ms :body")
+);
 
 let persons = [
   {
