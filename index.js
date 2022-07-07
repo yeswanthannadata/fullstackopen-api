a8a1d6f53baab353d1ecdfd10935d418263eb24e
Yeshu <yeswanth@flutura.com>
2022-07-07 19:19:35 +0530

    phonebook backend step9 complete

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
