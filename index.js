d5c873e16f83e9403760ddb82772783fec5bd2d2
Yeshu <yeswanth@flutura.com>
2022-07-07 19:25:07 +0530

    added port in env

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.get("/info", (request, response) => {
   response.send(htmlResponse);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
