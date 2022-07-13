08c0a2ca725201000eaec93a9ccde6e2d07b98bb
yeswanthannadata <annadatayeswanth@gmail.com>
2022-07-13 20:52:44 +0530

    phonebook database step 3,4,5 and 6 changes

--- index.js
+++ index.js
@@ -20,13 +20,15 @@ app.use(
   morgan(":method :url :status :res[Content-Length] :response-time ms :body")
 );
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => {
-    response.status(200).json(persons);
-  });
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.status(200).json(persons);
+    })
+    .catch((err) => next(err));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   const person = new Person({
@@ -34,32 +36,63 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.status(200).json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.status(200).json(savedPerson);
+    })
+    .catch((err) => next(err));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.status(200).json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.status(200).json(person);
+    })
+    .catch((err) => next(err));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  Person.findOneAndDelete({ _id: request.params.id }).then((result) => {
-    response.status(204).end();
-  });
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((err) => next(err));
 });
 
-app.get("/info", (request, response) => {
-  Person.find({}).then((persons) => {
-    const htmlResponse = `
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.status(200).json(updatedPerson);
+    })
+    .catch((err) => next(err));
+});
+
+app.get("/info", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      const htmlResponse = `
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
   `;
-    response.send(htmlResponse);
-  });
+      response.send(htmlResponse);
+    })
+    .catch((err) => next(err));
 });
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    response.status(400).send({ error: "malformed id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
